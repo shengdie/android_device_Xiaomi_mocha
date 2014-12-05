7ceff9d13881a3b3f3cfaf97def391e6374651e0
shengdie <shilishengdie@gmail.com>
2014-12-06 02:30:34 +0800

    upload file

---
+++ cm.mk
@@ -0,0 +1,22 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := mocha
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Xiaomi/mocha/device_mocha.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/mocha/mocha:4.4.4/KTU84P/4.12.05:user/release-keys" \
+    PRIVATE_BUILD_DESC="mocha-user 4.4.4 4.12.05 release-keys"
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mocha
+PRODUCT_NAME := cm_mocha
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := mocha
+PRODUCT_MANUFACTURER := Xiaomi
---
+++ device_mocha.mk
@@ -0,0 +1,25 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/Xiaomi/mocha/mocha-vendor.mk)
+
+#DEVICE_PACKAGE_OVERLAYS += device/Xiaomi/mocha/overlay
+
+#LOCAL_PATH := device/Xiaomi/mocha
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := full_mocha
+PRODUCT_DEVICE := mocha
+
+$(call inherit-product, device/Xiaomi/mocha-common/mocha.mk)
